3e5e7623f6cdeadb8a720e2d12353702a8c85c3a
iamkorniichuk <iamkorniichuk@gmail.com>
2023-08-06 14:31:29 +0300

    Add annotated fields to Theme

--- themes/models.py
+++ themes/models.py
@@ -2,6 +2,20 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
+class ThemeManager(models.Manager):
+    def get_queryset(self):
+        return (
+            super()
+            .get_queryset()
+            .annotate(
+                is_main=models.ExpressionWrapper(
+                    models.Q(parent__isnull=True),
+                    output_field=models.BooleanField(_("is main")),
+                )
+            )
+        )
+
+
 class Theme(models.Model):
     name = models.CharField(_("name"), max_length=64)
     parent = models.ForeignKey(
@@ -12,6 +26,8 @@ class Theme(models.Model):
         related_name="children",
         verbose_name=_("parent"),
     )
+
+    objects = ThemeManager()
 
     def __str__(self):
         return self.name
