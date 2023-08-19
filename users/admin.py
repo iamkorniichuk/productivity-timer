38767c97df12270a8eb870e1f11ea9f994a3ea47
iamkorniichuk <iamkorniichuk@gmail.com>
2023-08-19 15:43:19 +0300

    Set up User admin panel

---
+++ users/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ["pk", "email", "is_staff"]
+    ordering = ["email"]
+    add_fieldsets = [
+        (None, {"fields": ["email", "password1", "password2", "is_staff"]}),
+    ]
+    fieldsets = [
+        (None, {"fields": ["email", "password", "is_staff"]}),
+    ]
