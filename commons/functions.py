ed70177cd486b9efc638af899bbb698903c06fbb
iamkorniichuk <iamkorniichuk@gmail.com>
2023-08-24 13:00:29 +0300

    Add descriptions to SQL functions

--- commons/functions.py
+++ commons/functions.py
@@ -4,12 +4,16 @@ from django.db.models.lookups import Exact
 
 
 class NonAggregateCount(models.Count):
+    """Doesn't aggregate rows, unlike built-in Count function"""
+
     contains_aggregate = False
 
 
 class TimeUnitToDuration(Cast):
+    """Converts text fields to duration using Cast function.
+    Expresses 'quarter' as '3 months.'"""
+
     def __init__(self, expression, output_field=models.DurationField()):
-        # TODO: Accept a number of time units
         expression = models.Case(
             models.When(Exact(expression, "quarter"), then=models.Value("3 months")),
             default=Concat(
@@ -20,13 +24,16 @@ class TimeUnitToDuration(Cast):
         super().__init__(expression, output_field)
 
 
-# Accepts kind argument of models.F type unlike built-in function Trunc
 class StartOf(models.Func):
+    """Accepts 'kind' argument as field, unlike built-in Trunc function."""
+
     def as_postgresql(self, compiler, connection):
         return super().as_sql(compiler, connection, function="DATE_TRUNC")
 
 
 class EndOf(StartOf):
+    """Returns start of the next time unit's cycle."""
+
     def __init__(self, *expressions, output_field=None, **extra):
         expressions = list(expressions)
         expressions[1] += TimeUnitToDuration(expressions[0])
