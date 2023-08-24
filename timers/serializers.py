a63974f6cda1b31fa8c9e0c769b9be9a75e3dab9
iamkorniichuk <iamkorniichuk@gmail.com>
2023-08-24 20:14:12 +0300

    Add annotated fields to Timer serializer

--- timers/serializers.py
+++ timers/serializers.py
@@ -4,6 +4,19 @@ from .models import Timer
 
 
 class TimerSerializer(serializers.ModelSerializer):
+    is_datetime_set = serializers.BooleanField(required=False)
+    datetime = serializers.DateTimeField(required=False)
+    is_ended = serializers.BooleanField(required=False)
+    duration = serializers.DurationField(required=False)
+    is_completed = serializers.BooleanField(required=False)
+
     class Meta:
         model = Timer
         fields = "__all__"
+        read_only_fields = [
+            "is_datetime_set",
+            "datetime",
+            "is_ended",
+            "duration",
+            "is_completed",
+        ]
