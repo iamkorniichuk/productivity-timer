b7ff5d29b93d48e9b10f45c50aeea101647baa5c
iamkorniichuk@gmail.com <iamkorniichuk@gmail.com>
2023-09-10 12:01:25 +0300

    Refactor PK related field with representation

--- commons/serializers.py
+++ commons/serializers.py
@@ -9,20 +9,19 @@ class CurrentUserDefault:
 
 
 class SerializerRepresentationMixin:
-    def __init__(self, serializer_class, queryset=None, *args, **kwargs):
+    def __init__(self, serializer_class, *args, **kwargs):
         self.serializer_class = serializer_class
+        self.queryset = kwargs.pop("queryset", None)
         super().__init__(*args, **kwargs)
-        self._queryset = queryset
 
     def get_queryset(self):
-        return self._queryset or self.serializer_class.Meta.model._default_manager.all()
+        return self.queryset or self.serializer_class.Meta.model._default_manager.all()
 
-    def to_representation(self, pk):
-        data = {"pk": str(pk)}  # TODO: To refactor
-        obj = self.get_queryset().get(**data)
+    def to_representation(self, value):
+        pk = super().to_representation(value)
+        obj = self.get_queryset().get(pk=pk)
         serializer = self.serializer_class()
-        a = serializer.to_representation(obj)
-        return a
+        return serializer.to_representation(obj)
 
 
 # TODO: Fix declaring read_only in meta doesn't work for this type
