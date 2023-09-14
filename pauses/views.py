9011ecb573c811ca3d3e3d647b082edc7d848c85
iamkorniichuk@gmail.com <iamkorniichuk@gmail.com>
2023-09-14 12:58:29 +0300

    Add auto populate end fields

--- pauses/views.py
+++ pauses/views.py
@@ -1,3 +1,4 @@
+from django.utils import timezone
 from rest_framework import generics
 
 from .serializers import PauseSerializer
@@ -9,6 +10,13 @@ class PauseList(generics.ListCreateAPIView):
     queryset = Pause.objects.all()
 
 
+# TODO: Add description about update `end`
 class PauseDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = PauseSerializer
     queryset = Pause.objects.all()
+
+    def update(self, request, *args, **kwargs):
+        if not request.data:
+            request.data["end"] = timezone.now()
+            print(request.data)
+        return super().update(request, *args, **kwargs)
--- timers/views.py
+++ timers/views.py
@@ -1,4 +1,6 @@
+from django.utils import timezone
 from rest_framework import generics
+
 from commons.views import UserRelatedObjectsMixin
 
 from .serializers import TimerSerializer
@@ -10,6 +12,13 @@ class TimerList(UserRelatedObjectsMixin, generics.ListCreateAPIView):
     queryset = Timer.objects.all()
 
 
+# TODO: Add description about update `end`
 class TimerDetail(UserRelatedObjectsMixin, generics.RetrieveUpdateDestroyAPIView):
     serializer_class = TimerSerializer
     queryset = Timer.objects.all()
+
+    def update(self, request, *args, **kwargs):
+        if not request.data:
+            request.data["end"] = timezone.now()
+            print(request.data)
+        return super().update(request, *args, **kwargs)
